4dac3928ca489a8f17c4ca1179598d026ce3af82
Julie Quinn <jmq2134@gmail.com>
2017-08-06 19:13:48 -0400

    html stuff

--- db/schema.sql
+++ db/schema.sql
@@ -11,7 +11,5 @@ CREATE TABLE `burgers` (
     PRIMARY KEY (ID)
 );
 
-INSERT INTO burgers (burger_name, devoured) VALUES ('Gourdon-Hamsey Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Poutine on the Ritz Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Lets Give Em Something Shiitake Bout Burger', false);
+
 
