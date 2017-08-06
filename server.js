9fd3677eb1695de2f304cab0233b904e66ade8d1
Julie Quinn <jmq2134@gmail.com>
2017-08-06 18:31:43 -0400

    Clone over watchlist template

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+var app = express();
+var port = 3000;
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(__dirname + "/public"));
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+// Handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+// Routes
+let routes = require('./controllers/movies.js');
+
+app.use('/', routes);
+
+// Check port
+app.listen(port);
