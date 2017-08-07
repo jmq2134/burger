a64f72b5e07049f8a145954e71b571c83f53e3f3
Julie Quinn <jmq2134@gmail.com>
2017-08-07 09:10:13 -0400

    Everything working, needs styling

--- config/orm.js
+++ config/orm.js
@@ -80,5 +80,5 @@ var orm = {
   }
 };
 
-// Export the orm object for the model (cat.js).
+// Export the orm object for the model (burger.js).
 module.exports = orm;
--- server.js
+++ server.js
@@ -17,14 +17,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
 
-// Set Handlebars.
+// Set Handlebars
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/catsController.js");
+var routes = require("./controllers/burgersController.js");
 
 app.use("/", routes);
 
