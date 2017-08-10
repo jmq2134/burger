5df10496cd8286bc821ede12ce6ab71a5c1e46bc
Julie Quinn <jmq2134@gmail.com>
2017-08-09 21:09:11 -0400

    sql update

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  // port: 3306,
+  port: 3306,
   host: "us-cdbr-iron-east-05.cleardb.net",
   user: "b7c6cc101f6679",
   password: "c3f4a9b5",
@@ -22,9 +22,9 @@ connection.connect(function(err) {
 module.exports = connection;
 
 // CLEARDB_DATABASE_URL: 
-// mysql://be1ff39a325763:0a069e12@
+//mysql://b7c6cc101f6679:c3f4a9b5@us-cdbr-iron-east-05.cleardb.net/heroku_ce86b77c480e4fc?reconnect=true
 
 // DB_HOST: us-cdbr-iron-east-05.cleardb.net
-// DB_DATABASE: heroku_543994320c3169f
-// DB_USERNAME: be1ff39a325763
-// DB_PASSWORD: 0a069e12
+// DB_DATABASE: heroku_ce86b77c480e4fc
+// DB_USERNAME: b7c6cc101f6679
+// DB_PASSWORD: c3f4a9b5
