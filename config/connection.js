8dbf68819cb65583bf82ce2549815436f178cfb2
Julie Quinn <jmq2134@gmail.com>
2017-08-09 21:02:26 -0400

    connect heroku to sql

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,10 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "Jujupop12",
-  database: "burgers_db"
+  host: "us-cdbr-iron-east-05.cleardb.net",
+  user: "be1ff39a325763",
+  password: "0a069e12",
+  database: "heroku_543994320c3169f"
 });
 
 // Make connection
@@ -20,3 +20,11 @@ connection.connect(function(err) {
 
 // Export connection for our ORM to use
 module.exports = connection;
+
+// CLEARDB_DATABASE_URL: 
+// mysql://be1ff39a325763:0a069e12@
+
+// DB_HOST: us-cdbr-iron-east-05.cleardb.net
+// DB_DATABASE: heroku_543994320c3169f
+// DB_USERNAME: be1ff39a325763
+// DB_PASSWORD: 0a069e12
