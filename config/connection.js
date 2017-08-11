86de3a9f121b4713bcd997bae75d9e35fac11e89
Julie Quinn <jmq2134@gmail.com>
2017-08-10 20:22:59 -0400

    heroku push

--- config/connection.js
+++ config/connection.js
@@ -11,16 +11,16 @@ var mysql = require("mysql");
 
 var connection;
 
-if (process.env.CLEARDB_DATABASE_URL) {
-    connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
-} else {
-    connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'burgers_db'
-    });
-};
+// if (process.env.CLEARDB_DATABASE_URL) {
+connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+// } else {
+//     connection = mysql.createConnection({
+//         host: 'localhost',
+//         user: 'root',
+//         password: '',
+//         database: 'burgers_db'
+//     });
+// };
 
 // Make connection
 connection.connect(function(err) {
